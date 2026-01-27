b1d22410f3444e28f995973186ecccdcb5f8dc7b
toska27 <nemanjatoskic58@gmail.com>
2026-01-27 17:03:07 +0100

    added name in chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -62,7 +62,10 @@ export default function Chat({ apartment, lang }) {
 
   return (
     <div className="max-w-lg mt-5 mb-5 mx-auto bg-[#0D1B2A] rounded-2xl shadow-xl p-6">
-      <h1 className="text-center text-3xl font-bold mb-4 text-[#ffffff]">
+      <h1 className="text-center text-3xl font-bold mb-2 text-white tracking-wide">
+        {apartment.name}
+      </h1>
+      <h1 className="text-center text-3xl font-bold mb-4 text-[#ffffff] tracking-wide">
         {lang === "sr" ? "AI Asistent Apartmana" : "AI Apartment Assistant"}
       </h1>
 
